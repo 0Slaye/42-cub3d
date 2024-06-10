b30d388fe029ae7585ee2b3f8666fbe2f07bd487
Slaye <uduterrage@gmail.com>
2024-06-10 14:41:08 +0200

    Add: checks for argv and map.cub

--- cub3d/sources/debug.c
+++ cub3d/sources/debug.c
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   commons.h                                          :+:      :+:    :+:   */
+/*   debug.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/05 17:10:53 by slaye             #+#    #+#             */
-/*   Updated: 2024/06/10 14:31:16 by slaye            ###   ########.fr       */
+/*   Created: 2024/06/10 14:24:53 by slaye             #+#    #+#             */
+/*   Updated: 2024/06/10 14:28:41 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COMMONS_H
-# define COMMONS_H
+#include "commons.h"
 
-# include <stdio.h>
-# include <unistd.h>
-# include "../sources/libft/libft.h"
-
-# define TRUE 1
-# define FALSE 0
-# define ERR_ARGS "cub3d: Wrong parameters."
-
-void	ft_error(char *value);
-
-# endif
+void	ft_error(char *value)
+{
+	ft_putendl_fd("Error", STDERR_FILENO);
+	ft_putendl_fd(value, STDERR_FILENO);
+}
--- cub3d/sources/main.c
+++ cub3d/sources/main.c
@@ -6,14 +6,30 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:09:52 by slaye             #+#    #+#             */
-/*   Updated: 2024/06/05 17:22:52 by slaye            ###   ########.fr       */
+/*   Updated: 2024/06/10 14:38:32 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commons.h"
 
-int	main(void)
+int	is_args(int argc, char **argv)
 {
-	ft_putendl_fd("Working libft.", 1);
+	if (argc != 2)
+		return (ft_error(ERR_ARGS), FALSE);
+	else if (ft_strrchr(argv[1], '.') == FALSE)
+		return (ft_error(ERR_ARGS), FALSE);
+	else if (ft_strlen(ft_strrchr(argv[1], '.')) != 4)
+		return (ft_error(ERR_ARGS), FALSE);
+	else if (ft_strncmp(ft_strrchr(argv[1], '.'), ".cub", 4) != FALSE)
+		return (ft_error(ERR_ARGS), FALSE);
+	return (TRUE);
+}
+
+int	main(int argc, char **argv)
+{
+	if (is_args(argc, argv) == FALSE)
+		return (1);
+	else
+		return (0);
 	return (0);
 }
