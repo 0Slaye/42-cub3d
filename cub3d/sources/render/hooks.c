74978884dc1bb0e975856518b279eeadb9e1995e
Matthieu De Langhe <mde-lang@k2r3p9.42mulhouse.fr>
2024-06-18 18:37:47 +0200

    floor & ceiling / collisions

--- cub3d/sources/render/hooks.c
+++ cub3d/sources/render/hooks.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:10:10 by slaye             #+#    #+#             */
-/*   Updated: 2024/06/18 18:07:24 by slaye            ###   ########.fr       */
+/*   Updated: 2024/06/18 18:36:24 by mde-lang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 bool	check_collision(t_program *p, double prediction_y, double prediction_x)
 {
-	if (p->map->grid[(int)floor(prediction_y)][(int)floor(prediction_x)] == '1')
+	if (p->map->grid[(int)floor(prediction_y)][(int)floor(prediction_x)] == WALL)
 		return (true);
 	return (false);
 }
@@ -32,13 +32,13 @@ void	hooks(void *program)
 		mlx_close_window(mlx);
 		fexit(program, EXIT_SUCCESS, DB_END, STDIN_FILENO);
 	}
-	if (mlx_is_key_down(p->mlx, MLX_KEY_W) && (check_collision(p, p->player->y -= MOVE, p->player->x) == false))
+	if (mlx_is_key_down(p->mlx, MLX_KEY_W) && (check_collision(p, p->player->y - MOVE, p->player->x) == false))
 		p->player->y -= MOVE;
-	if (mlx_is_key_down(p->mlx, MLX_KEY_A) && (check_collision(p, p->player->y, p->player->x -= MOVE) == false))
+	if (mlx_is_key_down(p->mlx, MLX_KEY_A) && (check_collision(p, p->player->y, p->player->x - MOVE) == false))
 		p->player->x -= MOVE;
-	if (mlx_is_key_down(p->mlx, MLX_KEY_D) && (check_collision(p, p->player->y, p->player->x += MOVE) == false))
+	if (mlx_is_key_down(p->mlx, MLX_KEY_D) && (check_collision(p, p->player->y, p->player->x + MOVE) == false))
 		p->player->x += MOVE;
-	if (mlx_is_key_down(p->mlx, MLX_KEY_S) && (check_collision(p, p->player->y += MOVE, p->player->x) == false))
+	if (mlx_is_key_down(p->mlx, MLX_KEY_S) && (check_collision(p, p->player->y + MOVE, p->player->x) == false))
 		p->player->y += MOVE;
 	fc_printer(program);
 	raycasting(program);
