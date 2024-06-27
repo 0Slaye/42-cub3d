8a50df189368b3c0372f16d3ebd004e543bb2044
Slaye <uduterrage@gmail.com>
2024-06-27 21:13:42 +0200

    Fix: changing player move speed

--- cub3d/includes/commons.h
+++ cub3d/includes/commons.h
@@ -6,7 +6,7 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:10:53 by slaye             #+#    #+#             */
-/*   Updated: 2024/06/27 20:39:33 by slaye            ###   ########.fr       */
+/*   Updated: 2024/06/27 21:12:12 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@
 # define W_WIDTH 1024
 # define W_HEIGHT 512
 
-# define MOVE 0.1
-# define ROT 0.1
+# define MOVE 0.05
+# define ROT 0.05
 
 # define SCELL 1.0
 # define SQUARE 64
